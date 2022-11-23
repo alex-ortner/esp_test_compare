b590ce42e0919fc440eec102273fac5d5698c6b5
Alexander Ortner <alexander.ortner@student.tugraz.at>
2022-11-23 16:56:17 +0100

    #testrunner_is _running

--- comprunner.c
+++ comprunner.c
@@ -18,7 +18,7 @@
 #define IO_MAX_LINES 50
 #define SIZE_IO_LINE sizeof(struct io_comp_line)
 #define SIZE_IO_LINE_PTR sizeof(struct io_comp_line *)
-#define SIZE_IO_MAX_PTR SIZE_IO_LINE_PTR * IO_MAX_LINES
+#define SIZE_IO_MAX_PTR SIZE_IO_LINE_PTR *IO_MAX_LINES
 
 enum testCaseType
 {
@@ -88,17 +88,16 @@ int filter_io_lines(struct io_comp_line **io_lines, int line_count,
                     struct io_comp_line **out_io_lines,
                     enum io_line_type filter);
 
-
-
 int main(int argc, char **argv)
 {
   struct testCase test_case = {
       OrdIO, "test/10/io", "./a2", {"./a2", "E", NULL}, 0, 0, 0, 0.0f, 10.0f};
 
-  if(argc > 1){
+  if (argc > 1)
+  {
     strcpy(test_case.test_file, argv[1]);
   }
-
+  printf("Start process with file %s:\n",test_case.test_file);
   int test_res = test_runner(&test_case, out_first, &out_first_ptr);
   return 0;
 }
@@ -141,17 +140,25 @@ int strsrch(const char *str1, const char *str2, int buff_len)
 
 int test_runner(struct testCase *test, char *out_file, int *out_index)
 {
-  struct io_comp_line **io_lines = NULL;
-  struct io_comp_line **io_resps = NULL;
+  setbuf(stdout, NULL);
+  printf("-> Initialise Testrunner:\n");
+  struct io_comp_line **io_lines = (struct io_comp_line **)(malloc(SIZE_IO_MAX_PTR));
+  struct io_comp_line **io_resps = (struct io_comp_line **)(malloc(SIZE_IO_MAX_PTR));
+  if (io_lines == NULL || io_resps == NULL){
+    free(io_lines);
+    free(io_resps);
+    return -1;
+  }
+
   int line_count = get_io_lines(test, io_lines);
+  printf("-> got File Input:\n");
   if (line_count == -1)
   {
     free(io_lines);
     return 3;
   }
+  printf("-> start filtering:\n");
   int resp_count = filter_io_lines(io_lines, line_count, io_resps, line_in);
-  for (int i = 0; i < resp_count; i++)
-    printf("io %2d: %s", i, io_resps[i]->line);
 
   int f_stdin[2];
   int f_stdout[2];
@@ -159,7 +166,7 @@ int test_runner(struct testCase *test, char *out_file, int *out_index)
   if (pipe(f_stdin) == -1)
   {
     perror("Failed to open stdin pipe\n");
-
+    free(io_resps);
     free(io_lines);
     return 2;
   }
@@ -169,22 +176,22 @@ int test_runner(struct testCase *test, char *out_file, int *out_index)
     perror("Failed to open stdout pipe\n");
     close(f_stdin[0]);
     close(f_stdin[1]);
-
+    free(io_resps);
     free(io_lines);
     return 2;
   }
 
   pid_t pid = fork();
-  printf("Process %d uses in pipe %d -> %d\n", getpid(), f_stdin[0], f_stdin[1]);
-  printf("Process %d uses outpipe %d -> %d\n", getpid(), f_stdout[0], f_stdout[1]);
   if (pid == -1)
   {
     perror("fork failed");
     free(io_lines);
+    free(io_resps);
     return 1;
   }
   else if (pid == 0)
   {
+    printf("-> Child generated\n");
     setbuf(stdout, NULL);
     close(f_stdout[0]);
     close(f_stdin[1]);
@@ -195,6 +202,7 @@ int test_runner(struct testCase *test, char *out_file, int *out_index)
     close(f_stdout[1]);
     close(f_stdin[0]);
     free(io_lines);
+    free(io_resps);
     execvp(test->prog_file, test->args);
     perror("exec failed");
     return -2;
@@ -210,11 +218,12 @@ int test_runner(struct testCase *test, char *out_file, int *out_index)
   alarm(TIME_LIMIT); // install an alarm to be fired after TIME_LIMIT
 
   int ret_resp = io_pipe_handler(f_stdin, f_stdout, out_file, out_index,
-                                 io_lines, resp_count);
+                                 io_resps, resp_count);
   if (ret_resp != 0)
   {
     perror("response handler failed\n");
     free(io_lines);
+    free(io_resps);
     return ret_resp;
   }
   if (timeout == 0)
@@ -224,18 +233,20 @@ int test_runner(struct testCase *test, char *out_file, int *out_index)
   int result = waitpid(pid, &wstatus, WNOHANG);
   if (timeout && result == 0)
   {
+    free(io_resps);
+    free(io_lines);
     printf("alarm triggered\n");
 
     // child still running, so kill it
     printf("killing child\n");
     kill(pid, 9);
     wait(NULL);
-    free(io_lines);
+
     return -1;
   }
   else if (test_done)
   {
-    printf("Wstatus %d\n", wstatus);
+    printf("\n\nWstatus %d\n", wstatus);
     if (WIFEXITED(wstatus))
     {
       int status = WEXITSTATUS(wstatus);
@@ -244,6 +255,8 @@ int test_runner(struct testCase *test, char *out_file, int *out_index)
     }
   }
   free(io_lines);
+  free(io_resps);
+
   return 0;
 }
 
@@ -270,7 +283,7 @@ int io_pipe_handler(int f_stdin[2], int f_stdout[2], char *out_file,
   int ret;
   close(f_stdout[1]);
   close(f_stdin[0]);
-  printf("From Child\n\n");
+  printf("Pipe to Child\n\n");
 
   /* watch stdin for input */
   fds[0].fd = f_stdout[0];
@@ -301,7 +314,7 @@ int io_pipe_handler(int f_stdin[2], int f_stdout[2], char *out_file,
         close(f_stdin[1]);
         break;
       }
-      printf("%d < %s", f_stdin[1], io_resp[respNr]->line);
+      //printf("%d < %s", f_stdin[1], io_resp[respNr]->line);
       write(f_stdin[1], io_resp[respNr]->line, io_resp[respNr]->length);
       respNr++;
     }
@@ -329,15 +342,15 @@ int io_pipe_handler(int f_stdin[2], int f_stdout[2], char *out_file,
 int get_io_lines(struct testCase *test, struct io_comp_line **io_lines)
 {
   FILE *fptr = fopen(test->test_file, "r");
-
-  io_lines = (struct io_comp_line **)(malloc(SIZE_IO_MAX_PTR));
-  if(io_lines == NULL)
-    return -1;
+  printf("  -> Get Memory via Malloc \n");
   for (int i = 0; i < IO_MAX_LINES; i++)
     io_lines[i] = malloc(SIZE_IO_LINE);
+  printf("  -> Finished allocating \n");
 
   int lines = 0;
   char buff[LINE_BUFF_SIZE];
+
+
   while (fgets(buff, LINE_BUFF_SIZE, fptr) != NULL)
   {
     if (buff[0] == '>')
@@ -354,26 +367,27 @@ int get_io_lines(struct testCase *test, struct io_comp_line **io_lines)
       return -1;
     }
     int i = 0;
-    for (;
-         i + 2 < LINE_BUFF_SIZE && buff[i + 2] != '\n' && buff[i + 2] != '\0';
-         i++)
+    for (;i + 2 < LINE_BUFF_SIZE && buff[i + 2] != '\n' && buff[i + 2] != '\0'; i++)
     {
       io_lines[lines]->line[i] = buff[i + 2];
     }
     io_lines[lines]->line[i] = '\n';
     io_lines[lines]->length = i + 1;
+    for (i = i + 1;i < LINE_BUFF_SIZE; i++)
+    {
+      io_lines[lines]->line[i] = 0;
+    }
     lines++;
   }
   fclose(fptr);
   return lines;
 }
 
-int filter_io_lines(struct io_comp_line **io_lines, int line_count, 
-                    struct io_comp_line **out_io_lines, 
+int filter_io_lines(struct io_comp_line **io_lines, int line_count,
+                    struct io_comp_line **out_io_lines,
                     enum io_line_type filter)
 {
-  out_io_lines = (struct io_comp_line **)(malloc(SIZE_IO_MAX_PTR));
-  if(out_io_lines == NULL)
+  if (out_io_lines == NULL)
     return -1;
   for (int i = 0; i < IO_MAX_LINES; i++)
     out_io_lines[i] = (struct io_comp_line *)(malloc(SIZE_IO_LINE));
@@ -384,12 +398,16 @@ int filter_io_lines(struct io_comp_line **io_lines, int line_count,
     if (io_lines[i]->type == filter)
     {
       int j = 0;
-      for (; j < LINE_BUFF_SIZE && io_lines[i]->line[j] != '\n' && io_lines[i]->line[j] != '\0'; j++)
+      for (; j < io_lines[i]->length; j++)
       {
-        out_io_lines[lines]->line[j] = io_lines[lines]->line[j];
+        out_io_lines[lines]->line[j] = io_lines[i]->line[j];
       }
       out_io_lines[lines]->line[j] = '\n';
       out_io_lines[lines]->length = j + 1;
+      for (j = j + 1; j < io_lines[i]->length; j++)
+      {
+        out_io_lines[lines]->line[j] = 0;
+      }
       lines++;
     }
   }
